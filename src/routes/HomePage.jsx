3715cccd2ce77da890a1caad44612716b0b4739a
unknownburphy <chrisandjj1218@gmail.com>
2023-07-10 13:46:15 +0900

    feat: add routes

---
+++ src/routes/HomePage.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div id="work-area-wrapper" className="h-96">
+      <div id="work-area">
+        <div id="work-area-container-dropzone">
+          <div>title and subtitle</div>
+          <div>upload</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
