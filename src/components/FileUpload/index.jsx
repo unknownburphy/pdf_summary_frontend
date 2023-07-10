d8588782fec6f4ad7e3ae5ffb8c5434675f956d1
minhouu <tortor1403@gmail.com>
2023-07-10 22:26:12 +0900

    feat : pdf converter good

--- src/components/FileUpload/index.jsx
+++ src/components/FileUpload/index.jsx
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
+import { convertPdfToText } from "../Functions/pdfconverter";
 
 const FileUpload = () => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [fileUrl, setFileUrl] = useState(null);
 
   const handleFileChange = (e) => {
-    setSelectedFile(e.target.files[0]);
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      setFileUrl(event.target.result);
+      // convertPdfToText(fileUrl);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handleUpload = () => {
+    if (fileUrl == null) {
+      console.log("No file selected");
+    } else {
+      convertPdfToText(fileUrl);
+      // extractedChars가 추출된 txt. 이걸 동현이 함수에 넣으면 됨 ㅋ
+      // console.log(extractedChars);
+    }
   };
 
   return (
     <div>
       <input type="file" onChange={handleFileChange} className="bg-teal-200" />
-      <button onClick={() => console.log(selectedFile)}>Upload</button>
+      <button onClick={handleUpload}>Upload</button>
     </div>
   );
 };
