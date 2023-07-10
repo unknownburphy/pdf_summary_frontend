bb437d1e7fe7a3859895df6da0d24ea2cb78a2a1
unknownburphy <chrisandjj1218@gmail.com>
2023-07-10 20:43:36 +0900

    feat: add summary page

--- src/routes/SummaryPage.jsx
+++ src/routes/SummaryPage.jsx
@@ -1,5 +1,17 @@
 const Summary = () => {
-  return <div>Summary</div>;
+  return (
+    <div id="summary-area-wrapper" className="h-96">
+      <div className="h-36">Summary Completed!</div>
+      <div className="flex items-center justify-between w-full">
+        <div>example</div>
+        <div className="flex-col items-center justify-between">
+          <button>a</button>
+          <button>b</button>
+          <button>c</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Summary;
