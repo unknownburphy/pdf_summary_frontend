d4939871f6e5cb373480dfa12ac3368e9ab459df
unknownburphy <chrisandjj1218@gmail.com>
2023-07-10 20:43:16 +0900

    feat: add file upload

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,10 +1,17 @@
+import FileUpload from "../components/FileUpload";
+
 const Home = () => {
   return (
     <div id="work-area-wrapper" className="h-96">
       <div id="work-area">
         <div id="work-area-container-dropzone">
-          <div>title and subtitle</div>
-          <div>upload</div>
+          <div>
+            <div>title</div>
+            <div>subtitle</div>
+          </div>
+          <div>
+            <FileUpload />
+          </div>
         </div>
       </div>
     </div>
