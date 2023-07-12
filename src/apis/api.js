61d78e392e38f831949b7fbdc9f322225b6621d0
han <keroro19671967@gmail.com>
2023-07-12 19:11:06 +0900

    feat:add api.js file

---
+++ src/apis/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const test = (question) => {
+  const url = "https://api.openai.com/v1/completions";
+
+  const headers = {
+    Authorization: "Bearer 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",
+    "Content-Type": "application/json",
+  };
+
+  const data = {
+    model: "text-davinci-003",
+    prompt: question,
+    max_tokens: 500,
+  };
+
+  const response = axios.post(url, data, { headers });
+
+  return response;
+};
