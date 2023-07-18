7e17f8a076d4e624b20a8924e0a34075db874dfd
unknownburphy <chrisandjj1218@gmail.com>
2023-07-18 13:16:38 +0900

    feat: add data flow from App.js to HomePage and SummaryPage

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,6 +1,29 @@
-import FileUpload from "../components/FileUpload";
+import { useNavigate } from "react-router";
+import { useState } from "react";
 
-const Home = () => {
+const Home = ({ onReceiveSummary }) => {
+  const navigate = useNavigate();
+
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handleSubmit = () => {
+    // TODO:
+    // 1. extract text from pdf file (selected file)
+    // 2. make summary from extracted text
+    // 3. this will happen in loading page
+    // 4. if loading is completed, give summary data to App.js
+    // 5. Finally, navigate to the summary page
+    const dummySummary = "this is a dummy summary";
+
+    onReceiveSummary(dummySummary);
+
+    navigate("/summary");
+  };
+
   return (
     <div id="work-area-wrapper" className="h-96">
       <div id="work-area">
@@ -10,7 +33,12 @@ const Home = () => {
             <div>subtitle</div>
           </div>
           <div>
-            <FileUpload />
+            <input
+              type="file"
+              onChange={handleFileChange}
+              className="bg-teal-200"
+            />
+            <button onClick={handleSubmit}>Upload</button>
           </div>
         </div>
       </div>
