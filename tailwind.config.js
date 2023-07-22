ada416fa328c786635853bb03f6a851570b20742
kimdanran <ranawonder@gmail.com>
2023-07-22 15:52:09 +0900

    STYLE header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        key: "#00798C",
+      },
+    },
   },
   plugins: [],
 };
