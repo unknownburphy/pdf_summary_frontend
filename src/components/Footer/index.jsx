495ca5075f70194d638166e0b1b40d418dcf03df
daeguhighman <keroro19671967@gmail.com>
2023-07-29 15:00:37 +0900

    footer hide in summarypage

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,11 @@
+import { useLocation } from "react-router-dom";
 const Footer = () => {
+  const location = useLocation();
+  if (location.pathname === '/summary') return null;
   return (
     <div id="footer-wrapper" className="w-full">
       <div className="bg-gray-50 w-full h-80">
-        <div></div>
+        <div>hi</div>
         <span></span>
       </div>
     </div>
